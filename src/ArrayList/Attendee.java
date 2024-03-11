c988d86e4dd74537085d082fe3c8ad0d67b499e7
X95xy <160329173+X95xy@users.noreply.github.com>
2024-03-11 21:49:39 +0800

    Add files via upload

---
+++ src/ArrayList/Attendee.java
@@ -0,0 +1,67 @@
+package ArrayList;
+
+import java.util.Objects;
+
+public class Attendee {
+    private String name;
+    private char gender;
+    private String email;
+    private int age;
+
+    public Attendee(String name, char gender, String email, int age) {
+
+        this.name = Objects.requireNonNull(name, "Unknown");
+        this.gender = gender;
+        this.email = email;
+        this.age = age;
+
+    }
+
+    public String toString() {
+
+        return "name: " + this.name + " gender: " + this.gender + " email: " + this.email + " age: " + this.age;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+    public void setGender(char name) {
+
+        this.gender = name;
+
+    }
+
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+
+    public void setAge(int age) {
+
+        this.age = age;
+    }
+
+    public int getAge() {
+
+        return this.age;
+
+    }
+
+    public char getGender() {
+
+        return this.gender;
+    }
+
+    public String getName() {
+
+        return this.name;
+    }
+
+    public String getEmail() {
+
+        return this.email;
+    }
+}
